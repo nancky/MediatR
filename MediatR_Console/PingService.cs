7ab21c7914e09f1285a541d7b998f39e80149966
John Haynes <john.haynes@pulaski.kyschools.us>
2020-08-18 16:11:17 -0400

    working sample

--- MediatR_Console/PingService.cs
+++ MediatR_Console/PingService.cs
@@ -8,26 +8,23 @@ namespace MediatR_Console
     class PingService : IPingService
     {
         private readonly IMediator _mediator;
-        public PingService(IMediator mediator) 
+        public PingService(IMediator mediator)
         {
             _mediator = mediator;
         }
+
         public void Ping(string ip)
         {
             Console.WriteLine($"Pinging: {ip}");
             _mediator.Publish(new PingEvent($"Ping Service publishes notification PingEvent ({ip})"));
-            
-            Console.WriteLine("MediatR command LookupRequest returned: {0}", _mediator.Send(new LookupRequest()).Result);
+
+            Console.WriteLine($"MediatR command LookupRequest returned: {_mediator.Send(new LookupRequest()).Result}");
 
             LookupResult lookupResult = _mediator.Send(new LookupRequest2(ip)).Result;
-            if (lookupResult.Success)
+            if(lookupResult.Success)
                 Console.WriteLine($"MediatR command LookupRequest2 returned: {lookupResult.Name}");
             else
                 Console.WriteLine($"MediatR command LookupRequest2 error: {lookupResult.ErrorMessage}");
-
-
-            LookupResult lookupResult = _mediator.Send(new LookupRequest2(ip)).Result;
-
         }
     }
 }
